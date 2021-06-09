3a3fc3755028a0eb8d9a8dced94258fb07c4196c
Yaroslav Kuznietsov <YaroslavKuznietsov@EST-bielefeld.eu>
2021-06-09 11:30:14 +0200

    Added Mutex to the Program.cs to avoid multinstance start

--- WinS7Client/Program.cs
+++ WinS7Client/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,16 +9,43 @@ namespace WinS7Client
 {
     static class Program
     {
+
+        //static Mutex mutex = new Mutex(true, "{8F6F0AC4-B9A1-45fd-A8CF-72F04E6BDE8F}");
+        //[STAThread]
+        //static void Main()
+        //{
+        //    if (mutex.WaitOne(TimeSpan.Zero, true))
+        //    {
+        //        Application.EnableVisualStyles();
+        //        Application.SetCompatibleTextRenderingDefault(false);
+        //        Application.Run(new Form1());
+        //        mutex.ReleaseMutex();
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("only one instance at a time");
+        //    }
+        //}
+
+
+        static Mutex mutex = new Mutex(true, "{8F6F0AC4-B9A1-45fd-A8CF-72F04E6BDE8F}");
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
-            Application.Run(new MainForm());
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                //Application.Run(new Form1());
+                Application.Run(new MainForm()); 
+            }
+            //else
+            //{
+            //    MessageBox.Show("only one instance at a time");
+            //}
         }
     }
 }

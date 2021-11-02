35e7227e8a97d6c165d5a6f52f4c99eb90d52943
yaroslavkuznietsov <yaroslav.kuznietsov@gmail.com>
2021-11-02 16:34:31 +0200

    Added new class for operation times and params

---
+++ WinS7Library/Model/DATBetrieb.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinS7Library.Model
+{
+    // Defines that you want to serialize this class
+    [Serializable()]
+    class DATBetrieb : ISerializable
+    {
+        public int StdSollHE { get; set; }
+        public int StdIstHE { get; set; }
+        public int StdSollOB { get; set; }
+        public int StdIstOB { get; set; }
+        public int StdSollUN { get; set; }
+        public int StdIstUN { get; set; }
+
+
+        //ctor
+        public DATBetrieb() { }
+
+
+        // Serialization function (Stores Object Data in File)
+        // SerializationInfo holds the key value pairs
+        // StreamingContext can hold additional info
+        // but we aren't using it here
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+        }
+
+        // The deserialize function (Removes Object Data from File)
+        public DATBetrieb(SerializationInfo info, StreamingContext ctxt)
+        {
+        }
+    }
+}

7a8d07d19b19567400fe90c397465974b8e09455
yaroslavkuznietsov <yaroslav.kuznietsov@gmail.com>
2021-11-04 11:07:55 +0200

    Changed properties privacy to readonly + DB_DatBetrieb length fixed

--- WinS7Library/Model/CommData.cs
+++ WinS7Library/Model/CommData.cs
@@ -16,23 +16,25 @@ namespace WinS7Library.Model
         public int N { get; set; }
 
         //DB Numbers
-        public int DB_DAT_HE = 7000;
-        public int DB_DAT_Config = 7001;
-        public int DB_DAT_N2 = 7002;
-        public int DB_DAT_Werkzeug = 7003;
-        public int DB_DAT_MWerkzeug = 7004;
-        public int DB_Service_WKZ_Liste = 7005;
-        public int DB_Service_PlcToPc = 7006;
-        public int DB_Service_PcToPlc = 7007;
+        public readonly int DB_DAT_HE = 7000;
+        public readonly int DB_DAT_Config = 7001;
+        public readonly int DB_DAT_N2 = 7002;
+        public readonly int DB_DAT_Werkzeug = 7003;
+        public readonly int DB_DAT_MWerkzeug = 7004;
+        public readonly int DB_Service_WKZ_Liste = 7005;
+        public readonly int DB_Service_PlcToPc = 7006;
+        public readonly int DB_Service_PcToPlc = 7007;
+        public readonly int DB_DAT_Betrieb = 7008;
 
         //DB Length in bytes
-        public int DB_DAT_HE_Length = 240;
-        public int DB_DAT_Config_Length = 2796;
-        public int DB_DAT_N2_Length = 640;
-        public int DB_DAT_Werkzeug_Length = 260; // v.4 Changes for customer 28.04.2021 -> 40 Signs
-        public int DB_DAT_MWerkzeug_Length = 40;
-        public int DB_Service_WKZ_Liste_Length = 5632;   // v.4 Changes for customer 28.04.2021 -> 40 Signs
-        public int DB_Service_PlcToPc_Length = 122;  // v.4 Changes for customer 28.04.2021 -> 40 Signs
-        public int DB_Service_PcToPlc_Length = 6;
+        public readonly int DB_DAT_HE_Length = 240;
+        public readonly int DB_DAT_Config_Length = 2796;
+        public readonly int DB_DAT_N2_Length = 640;
+        public readonly int DB_DAT_Werkzeug_Length = 260; // v.4 Changes for customer 28.04.2021 -> 40 Signs
+        public readonly int DB_DAT_MWerkzeug_Length = 40;
+        public readonly int DB_Service_WKZ_Liste_Length = 5632;   // v.4 Changes for customer 28.04.2021 -> 40 Signs
+        public readonly int DB_Service_PlcToPc_Length = 126;  // v.4 Changes for customer 28.04.2021 -> 40 Signs
+        public readonly int DB_Service_PcToPlc_Length = 6;
+        public readonly int DB_DAT_Betrieb_Length = 24;
     }
 }

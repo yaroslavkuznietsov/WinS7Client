5c273157acfe09b063960c83c4b51f1539ccf4be
Yaroslav <yaroslav.kuznietsov@gmail.com>
2023-03-05 22:33:34 +0200

    add excel worksheet styl for data

--- WinS7Library/Helper/Exporter/ProcessDataExcelExporter.cs
+++ WinS7Library/Helper/Exporter/ProcessDataExcelExporter.cs
@@ -3,6 +3,7 @@ using OfficeOpenXml.Style;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Drawing;
 using System.IO;
 using WinS7Library.Files;
 using WinS7Library.Model.Export;
@@ -86,7 +87,7 @@ namespace WinS7Library.Helper.Exporter
             }
         }
 
-        //obsolete up to set header from DataTable but left for a while
+        //obsolete up to set header from DataTable but left for a while as alternative brut force
         private static void SetProcessDataHeader(ExcelWorksheet worksheet)
         {
             
@@ -117,17 +118,50 @@ namespace WinS7Library.Helper.Exporter
             var rowCount = worksheet.Dimension.Rows;
             var colCount = worksheet.Dimension.Columns;
 
+            // header style
             worksheet.Cells[1, 1, rowCount, colCount].Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
             worksheet.Cells[1, 1, rowCount, colCount].AutoFitColumns();
             worksheet.Cells[1, 1, 1, colCount].Style.Font.Bold = true;
 
-            //worksheet.Cells[2, 1, 2, colCount].Style.Fill.PatternType = ExcelFillStyle.Solid;
-            //worksheet.Cells[2, 1, 2, colCount].Style.Fill.BackgroundColor.SetColor(Color.LightGray);
-            //worksheet.Cells[3, 1, 3, colCount].Style.Fill.PatternType = ExcelFillStyle.Solid;
-            //worksheet.Cells[3, 1, 3, colCount].Style.Fill.BackgroundColor.SetColor(Color.White);
+            Color evenRowColor = System.Drawing.ColorTranslator.FromHtml("#f4f4f4");    //gray f4f4f4 e9e9e9
+            Color oddRowColor = System.Drawing.ColorTranslator.FromHtml("#b3cfb0");     //green
 
-            //worksheet.Cells[2, 1, 2, colCount].Style.Border.BorderAround(ExcelBorderStyle.Thin, Color.LightGray);
-            //worksheet.Cells[2, 1, 3, colCount].Style.Border.BorderAround(ExcelBorderStyle.Thin, Color.LightGray);
+            //data style
+            for (int i = 2; i <= rowCount; i++)
+            {
+                using (var range = worksheet.Cells[i, 1, i, colCount])
+                {
+                    if (i % 2 == 0)
+                    {
+                        range.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                        range.Style.Fill.BackgroundColor.SetColor(evenRowColor);
+
+                        range.Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                        range.Style.Border.Top.Color.SetColor(Color.Gray);
+                        range.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                        range.Style.Border.Right.Color.SetColor(Color.Gray);
+                        range.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                        range.Style.Border.Bottom.Color.SetColor(Color.Gray);
+                        range.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                        range.Style.Border.Left.Color.SetColor(Color.Gray);
+                    }
+                    else
+                    {
+                        range.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                        range.Style.Fill.BackgroundColor.SetColor(oddRowColor);
+
+                        range.Style.Border.BorderAround(ExcelBorderStyle.Thin);
+                        range.Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                        range.Style.Border.Top.Color.SetColor(Color.Gray);
+                        range.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                        range.Style.Border.Right.Color.SetColor(Color.Gray);
+                        range.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                        range.Style.Border.Bottom.Color.SetColor(Color.Gray);
+                        range.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                        range.Style.Border.Left.Color.SetColor(Color.Gray);
+                    }
+                }
+            }
 
             worksheet.View.FreezePanes(2, 1);
         }
